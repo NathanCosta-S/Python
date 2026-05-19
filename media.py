ab1e0026836560b498227c38e3d4eff0ee1ed2a6
Notbook <nathancosta73@outlook.com>
2026-05-19 09:38:26 -0300

    Adicionada verificação de aprovação do aluno

--- media.py
+++ media.py
@@ -2,5 +2,10 @@ nome = input("Digite o nome do aluno: ")
 nota1 = float(input("Digite a primeira nota: "))
 nota2 = float(input("Digite a segunda nota: "))
 media = (nota1 + nota2) / 2
+if media >= 6:
+    situacao = "Aprovado"
+else:
+    situacao = "Reprovado"
 print(f"Aluno: {nome}")
 print(f"Média: {media}")
+print(f"Situação: {situacao}")
